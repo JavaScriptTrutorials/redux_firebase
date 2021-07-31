d78f6ec95cca28871d17ab720d267e74c20a1d29
Marian Nociar <majo.nociar@gmail.com>
2021-07-31 18:55:37 +0200

    setting up firebase to get access to getFirebase & getFirestore functions to communicate with our firebaseAPI

--- src/index.js
+++ src/index.js
@@ -6,8 +6,18 @@ import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { compose } from 'redux';
+import fbConfig from './config/fbConfig';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, 
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig),
+  ) 
+);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -7,7 +7,7 @@ export const createProject = (project) => {
     };
     */
     // but when we use thunk, we return a function
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         // make async call to db
         dispatch({type: 'CREATE_PROJECT', project})
     };
