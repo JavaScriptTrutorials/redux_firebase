032ca52d600329e23e246ecd5e027902a19464e7
Marian Nociar <majo.nociar@gmail.com>
2021-07-31 10:41:25 +0200

    creating dashboard component with subcomponents (notifications & ProjectList)

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,29 @@
+const ProjectList = () => {
+    return (
+        <div className="projectList section">
+            <div className="card z-depth-0 project-sumarry">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the Marian</p>
+                    <p className="grey-text">3rd September, 9am</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-sumarry">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the Marian</p>
+                    <p className="grey-text">3rd September, 9am</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-sumarry">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the Marian</p>
+                    <p className="grey-text">3rd September, 9am</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default ProjectList;
