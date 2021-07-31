ed91ebcabe054ea6814a757e27e1c28357ccddfb
Marian Nociar <majo.nociar@gmail.com>
2021-07-31 15:09:24 +0200

    adding redux with multiple reducers

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import ProjectList from '../projects/ProjectList';
 import Notifications from './Notifications';
+import {connect} from 'react-redux';
 
 class Dashboard extends Component{
     render(){
+        console.log(this.props);
         return (
             <div className="dashboard container">
                 <div className="row">
@@ -21,4 +23,10 @@ class Dashboard extends Component{
     }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+    return{
+        projects: state.project.projects
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard);
