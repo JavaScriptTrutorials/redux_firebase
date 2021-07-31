3fb25e107f0d1d1a308def5a01478144b0f72d13
Marian Nociar <majo.nociar@gmail.com>
2021-07-31 10:47:42 +0200

    creating projectSummary component to be more modular

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,27 +1,12 @@
+import ProjectSummary from "./ProjectSummary"
+
 const ProjectList = () => {
     return (
         <div className="projectList section">
-            <div className="card z-depth-0 project-sumarry">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Marian</p>
-                    <p className="grey-text">3rd September, 9am</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-sumarry">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Marian</p>
-                    <p className="grey-text">3rd September, 9am</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-sumarry">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Marian</p>
-                    <p className="grey-text">3rd September, 9am</p>
-                </div>
-            </div>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
         </div>
     )
 };
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-sumarry">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by the Marian</p>
+                <p className="grey-text">3rd September, 9am</p>
+            </div>
+        </div>
+    )
+};
+
+export default ProjectSummary;
