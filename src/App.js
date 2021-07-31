855757b800199b9746d0d102fd45ccafcb306982
Marian Nociar <majo.nociar@gmail.com>
2021-07-31 11:38:02 +0200

    creating login and signup components without server connections

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
 import ProjectDetails from './components/projects/ProjetDetails';
@@ -11,6 +13,8 @@ function App() {
           <Switch>
             <Route exact path='/'><Dashboard/></Route>
             <Route path='/project/:id' component={ProjectDetails}/>
+            <Route path='/signin' component={SignIn}/>
+            <Route path='/signup' component={SignUp}/>
           </Switch>
       </div>
     </Router>
